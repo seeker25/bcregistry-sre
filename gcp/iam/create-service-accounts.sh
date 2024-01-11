f471780b5974fc6ef21227e1375c6c4004750c1f
pwei1018 <patrick.wei@gov.bc.ca>
2024-01-11 11:27:31 -0800

    Update Notify API OCP setup.

--- gcp/iam/create-service-accounts.sh
+++ gcp/iam/create-service-accounts.sh
@@ -1,29 +1,48 @@
 
-export PROJECT_ID=
-gcloud config set project ${PROJECT_ID}
+#!/bin/bash
 
-SERVICE_NAME="api"
-ROLE_NAME="role$SERVICE_NAME"
-SA_NAME="sa-$SERVICE_NAME"
-SA_FULL_NAME="$SA_NAME@${PROJECT_ID}.iam.gserviceaccount.com"
-SA_DESCRIPTION="Service Account for running $SERVICE_NAME services"
-SA_ROLE="projects/${PROJECT_ID}/roles/$ROLE_NAME"
+declare -a environments=("dev" "test" "tools" "prod" "integration" "sandbox")
+declare -a projects=("a083gt" "mvnjri" "gtksf3" "yfjq17" "c4hnrd" "k973yf" "yfthig" "eogruh")
+declare -a services=("api" "job" "queue")
 
-if [[ -z `gcloud iam roles describe $ROLE_NAME --project=${PROJECT_ID} --verbosity=none`]]
-then
-    gcloud iam roles create $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$SERVICE_NAME.yaml
-else
-    gcloud iam roles update $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$SERVICE_NAME.yaml
-fi
+for ev in "${environments[@]}"
+do
+    for ns in "${projects[@]}"
+    do
+        echo "project: $ns-$ev"
+        PROJECT_ID=$ns-$ev
 
-## API service account
-gcloud iam service-accounts create $SA_NAME \
-    --description="$SA_DESCRIPTION" \
-    --display-name="$SA_NAME"
+        if [[ ! -z `gcloud projects describe ${PROJECT_ID} --verbosity=none` ]]; then
+            gcloud config set project ${PROJECT_ID}
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:$SA_FULL_NAME" \
-    --role="$SA_ROLE"
+            for se in "${services[@]}"
+            do
+                ROLE_NAME="role$se"
+                SA_NAME="sa-$se"
+                SA_FULL_NAME="$SA_NAME@${PROJECT_ID}.iam.gserviceaccount.com"
+                SA_DESCRIPTION="Service Account for running $se services"
+                SA_ROLE="projects/${PROJECT_ID}/roles/$ROLE_NAME"
 
-gcloud iam service-accounts list --filter $SA_NAME
-gcloud iam roles describe $ROLE_NAME --project=${PROJECT_ID}
+                if [[ -z `gcloud iam roles describe $ROLE_NAME --project=${PROJECT_ID} --verbosity=none` ]]; then
+                    gcloud iam roles create $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$se.yaml
+                else
+                    gcloud iam roles update $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$se.yaml
+                fi
+
+                if [[ -z `gcloud iam service-accounts describe $SA_FULL_NAME --project=${PROJECT_ID} --verbosity=none` ]]; then
+                    ## API service account
+                    gcloud iam service-accounts create $SA_NAME \
+                        --description="$SA_DESCRIPTION" \
+                        --display-name="$SA_NAME"
+                fi
+
+                gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+                    --member="serviceAccount:$SA_FULL_NAME" \
+                    --role="$SA_ROLE"
+
+                #gcloud iam service-accounts list --filter $SA_NAME
+                #gcloud iam roles describe $ROLE_NAME --project=${PROJECT_ID}
+            done
+        fi
+    done
+done
