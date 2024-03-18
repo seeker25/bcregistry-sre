41ff3ef520b1b1ebce7ddf597e8f845fda031f86
pwei1018 <patrick.wei@gov.bc.ca>
2024-03-18 11:21:48 -0700

    Update roles scripts.

--- gcp/iam/application-roles.sh
+++ gcp/iam/application-roles.sh
@@ -3,7 +3,7 @@
 
 declare -a environments=("dev" "test" "tools" "prod" "integration" "sandbox")
 declare -a projects=("a083gt" "mvnjri" "gtksf3" "yfjq17" "c4hnrd" "k973yf" "yfthig" "eogruh")
-declare -a services=("api" "job" "queue")
+declare -a services=("api" "job" "queue" "cdcloudrun")
 
 for ev in "${environments[@]}"
 do
@@ -22,6 +22,12 @@ do
                 SA_FULL_NAME="$SA_NAME@${PROJECT_ID}.iam.gserviceaccount.com"
                 SA_DESCRIPTION="Service Account for running $se services"
                 SA_ROLE="projects/${PROJECT_ID}/roles/$ROLE_NAME"
+
+                if [[ -z `gcloud iam roles describe $ROLE_NAME --project=${PROJECT_ID} --verbosity=none` ]]; then
+                    gcloud iam roles create $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$se.yaml
+                else
+                    gcloud iam roles update $ROLE_NAME --quiet --project=${PROJECT_ID} --file=role-$se.yaml
+                fi
 
                 if [[ -z `gcloud iam service-accounts describe $SA_FULL_NAME --project=${PROJECT_ID} --verbosity=none` ]]; then
                     ## API service account
