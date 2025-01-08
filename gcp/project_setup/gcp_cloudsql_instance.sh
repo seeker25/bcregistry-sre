14c8f66eb41a25373767adf2aae0f9bc7ed5a2ec
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2025-01-08 14:04:17 -0800

    Audit log db grants (#210)
    * create log sink to keep track of db grant requests, since posts contain Jira ticket number
    
    * remove project id

--- gcp/project_setup/gcp_cloudsql_instance.sh
+++ gcp/project_setup/gcp_cloudsql_instance.sh
@@ -16,11 +16,20 @@ gcloud services enable servicenetworking.googleapis.com --project="${TARGET_PROJ
 
 # create the dataset only once
 # bq mk --location=$REGION --dataset ${HOST_PROJECT_ID}-${ENV}:cloudsql_audit_logs_${TAG}
+# bq mk --location=$REGION --dataset ${HOST_PROJECT_ID}-${ENV}:cloudsql_audit_grants_${TAG}
 
 gcloud logging sinks create cloudsql_audit_logs_${TAG} \
 bigquery.googleapis.com/projects/${HOST_PROJECT_ID}-${ENV}/datasets/cloudsql_audit_logs_${TAG} \
 --log-filter="logName=\"projects/${TARGET_PROJECT_ID}-${ENV}/logs/cloudaudit.googleapis.com%2Fdata_access\" AND resource.type=\"cloudsql_database\" AND protoPayload.serviceName=\"cloudsql.googleapis.com\" AND protoPayload.methodName=\"cloudsql.instances.query\"" \
 --use-partitioned-tables
+
+gcloud logging sinks create cloudsql_audit_grants_${TAG} \
+bigquery.googleapis.com/projects/${HOST_PROJECT_ID}-${ENV}/datasets/cloudsql_audit_grants_${TAG} \
+--log-filter="resource.labels.project_id=\"${TARGET_PROJECT_ID}-${ENV}\" \
+AND resource.type=\"cloud_run_revision\" \
+AND resource.labels.service_name=\"pam-request-grant-create\"" \
+--use-partitioned-tables
+
 
 gcloud sql instances create "${INSTANCE_NAME}-${TAG}" \
     --database-version=$POSTGRES_VERSION \
